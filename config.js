ea34f40f370f24479708db615c984d0248817bac
redjamhere <redjamhere@gmail.com>
2020-01-16 18:18:09 +0300

    navigation module

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  port: 4050,
+  htmlInput: 'src/index.pug',
+  styleInput: 'src/nice.styl',
+  buildOptions: {
+    htmlFileName: 'index.html'
+  }
+}
--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery';
-import popper from 'popper.js';
-import bootstrap from 'bootstrap';
-
-$(document).ready(() => {
-  $('body').css('background', 'red')
-})
