d51587ca78490a303edeec1d04b51a519811ad03
redjamhere <redjamhere@gmail.com>
2020-01-16 08:10:32 +0300

    unnamed commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,3 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 // import 'bootstrap/dist/css/bootstrap.min.css'
-
-
-import { Hello } from "./components/Hello/Hello"
-
-ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React"/>,
-  document.getElementById("root")
-)
