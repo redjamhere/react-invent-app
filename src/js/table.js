49a9f205b40de4a1439553ae1056506cba150e52
redjamhere <redjamhere@gmail.com>
2020-01-24 14:20:16 +0300

    fixed table

--- src/js/table.js
+++ src/js/table.js
@@ -4,14 +4,50 @@ import 'jquery-ui'
 let modalWindowOpenStatus = false
 let viewstatus = true
 
-//animated hover on open icon 
-
+//console.log clicked element
 $('body').click(function(event){
   console.log(event.target)
 })
 
+function openDepartModal() {
+  if (!modalWindowOpenStatus) {   
+
+
+    $('.depart-modal-window').animate({
+      height: "95%",
+      zIndex: "0"
+    }, 700)
+
+    $('.depart-modal-button').animate({
+      top: "0",
+      margin: "0"
+    }, 550)
+
+    $('.depart-modal-button').css({"flex-direction": "column-reverse"})
+    $('.depart-modal-button .open-icon').css({"transform": "rotate(180deg)"})
+  } else {
+
+    $('.depart-modal-window').animate({
+      height: "0%",
+      zIndex: "-1"
+    }, 700)
+
+    
+    $('.depart-modal-button').css({"top": '', "margin": ''})
+    $('.depart-modal-button').animate({
+      bottom: "0",
+      margin: "auto"
+    }, 500);
+
+    $('.depart-modal-button').css({"flex-direction": "column"});
+    $('.depart-modal-button .open-icon').css({"transform": "rotate(0deg)"});
+  }
+  modalWindowOpenStatus = !modalWindowOpenStatus;
+}
+
 $(document).ready(() => {
 
+  //depart modal open button hover script
   $('.depart-modal-button').mouseenter(() => {
     $('.depart-modal-button').animate({
       height: "37px"
@@ -31,44 +67,12 @@ $(document).ready(() => {
     }, 130)
   })
 
-  //open deaprt modal window
+  //open deaprt modal window open animatation
   $('.depart-modal-button .open-icon').click(() => {
-    if (!modalWindowOpenStatus) {   
-
-
-      $('.depart-modal-window').animate({
-        height: "95%",
-        zIndex: "0"
-      }, 700)
-
-      $('.depart-modal-button').animate({
-        top: "0",
-        margin: "0"
-      }, 550)
-
-      $('.depart-modal-button').css({"flex-direction": "column-reverse"})
-      $('.depart-modal-button .open-icon').css({"transform": "rotate(180deg)"})
-    } else {
-
-      $('.depart-modal-window').animate({
-        height: "0%",
-        zIndex: "-1"
-      }, 700)
-
-      
-      $('.depart-modal-button').css({"top": '', "margin": ''})
-      $('.depart-modal-button').animate({
-        bottom: "0",
-        margin: "auto"
-      }, 500)
-
-      $('.depart-modal-button').css({"flex-direction": "column"})
-      $('.depart-modal-button .open-icon').css({"transform": "rotate(0deg)"})
-    }
-    modalWindowOpenStatus = !modalWindowOpenStatus
+    openDepartModal()
   })
 
-  //viewstatus colored
+  //viewstatus checked colored (mb change to eye styled checkbox)
   $('.view-status').click(function() {
     if(viewstatus) {
       $(this).find('.depart-view-true').hide();
@@ -77,11 +81,13 @@ $(document).ready(() => {
       $(this).find('.depart-view-true').show();
       $(this).find('.depart-view-false').hide();
     }
-    viewstatus = !viewstatus
+    viewstatus = !viewstatus;
   })
 
+  //auto sized textarea
   $('tbody').on( 'change keyup keydown paste cut', 'textarea', function (){
     $(this).height(0).height(this.scrollHeight);
   }).find( 'textarea' ).change();
+
 })
 
